f5f20d19028af910e1143016c58babb730eb18b8
Darian Hickman <darian.hickman@mavgo.com>
2026-01-21 18:04:23 -0500

    Figuring out how to assess datatable view options

--- cmd/servelocal/main.go
+++ cmd/servelocal/main.go
@@ -15,6 +15,7 @@ func main() {
 	// Serve static files for the theme
 	http.Handle("/style1/", http.StripPrefix("/style1/", http.FileServer(http.Dir("themes/style1"))))
 	http.Handle("/extensions/", http.StripPrefix("/extensions/", http.FileServer(http.Dir("extensions"))))
+	http.Handle("/cssjs/", http.StripPrefix("/cssjs/", http.FileServer(http.Dir("cssjs"))))
 
 	http.Handle("/", srv)
 	log.Println("Serving local sqlite files from sample_data at http://localhost:8080")
---
+++ cssjs/default.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+    // do the work...
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+        const table = th.closest('table');
+        const tbody = table.querySelector('tbody');
+
+        // Remove active sort classes from other headers
+        table.querySelectorAll('th').forEach(header => {
+            if (header !== th) header.classList.remove('sort-asc', 'sort-desc');
+        });
+
+        // Toggle sort direction
+        const asc = !th.classList.contains('sort-asc');
+        th.classList.toggle('sort-asc', asc);
+        th.classList.toggle('sort-desc', !asc);
+
+        Array.from(tbody.querySelectorAll('tr'))
+            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), asc))
+            .forEach(tr => tbody.appendChild(tr));
+    })));
+});
